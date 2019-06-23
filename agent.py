bac0f2e78cca43ab1513fb8395f729d83c6f5ac3
hiroyukiinoue <hiroyukiinoue1444@gmail.com>
2019-06-23 14:26:02 +0900

    added agent and environment

---
+++ agent.py
@@ -0,0 +1,20 @@
+class Agent():
+    """
+    Args:
+        state
+
+    Return:
+        action
+    """
+    def __init__(self):
+        pass
+
+    def policy(self, state):
+        pass
+        return action
+
+    def learn(self, env):
+        # equivalent to fit
+        # learn Q: update Q
+        pass
+        return None
